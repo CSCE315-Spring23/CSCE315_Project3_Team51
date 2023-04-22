9cefc6ff0e708f39cd9a29abe1fa53777f2e9d85
Peter Just <56007515+PJust601@users.noreply.github.com>
2023-04-22 13:24:56 -0500

    Finished Employee.js

--- src/js/Employee.js
+++ src/js/Employee.js
@@ -2,11 +2,44 @@ const {pool} = require("./Conn.js");
 
 
 async function isEmployee(id) {
-    console.log("Work in progress");
+    try {
+        console.log('Checking if id ' + id + ' is an employee');
+        const res = await pool.query(
+            "SELECT employee_name FROM employees WHERE employee_id = $1",
+            [id]
+        );
+        if(res.rowCount > 0) {
+            console.log('Employee found: ' + res.rows[0].employee_name);
+            return res.rows[0];
+        } else {
+            console.error('Employee not found');
+            return -1;
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function isManager(id) {
-    console.log("Work in progress");
+    try {
+        console.log('Checking if id ' + id + ' is a manager');
+        const res = await pool.query(
+            "SELECT is_manager FROM employees WHERE employee_id = $1",
+            [id]
+        );
+        if(res.rowCount == 0) {
+            console.error('Employee not found');
+            return -1;
+        } else if(res.rows[0].is_manager) {
+            console.log('Manager found: ' + res.rows[0].employee_name);
+            return res.rows[0];
+        } else {
+            console.error('Non-manager found: ' + res.rows[0].employee_name);
+            return res.rows[0];
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 async function employeeInfo(id) {
@@ -22,6 +55,8 @@ async function employeeInfo(id) {
         console.error(error)
     }
 }
+
+// isEmployee(2);
 
 exports.isEmployee = isEmployee;
 exports.isManager = isManager;
