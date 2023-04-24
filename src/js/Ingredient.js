d6ee1bdf4e5c9051e46a65a49387a0cee481e64a
Peter Just <56007515+PJust601@users.noreply.github.com>
2023-04-24 16:11:52 -0500

    More report functions

--- src/js/Ingredient.js
+++ src/js/Ingredient.js
@@ -34,7 +34,25 @@ async function editInventory(ingredient, newQuantity = -1, minQuantity = -1) {
     }
 }
 
+async function signalNewDay() {
+    try {
+        console.log('Shifting inventory data');
+        pool.query(
+            "UPDATE inventory_data AS i SET prev_q7 = p.prev_q6, prev_q6 = p.prev_q5, prev_q5 = p.prev_q4, prev_q4 = p.prev_q3, prev_q3 = p.prev_q2, prev_q2 = p.prev_q1, prev_q1 = p.quantity " +
+            "FROM (SELECT ingredient_name, prev_q6, prev_q5, prev_q4, prev_q3, prev_q2, prev_q1, quantity FROM inventory_data)" +
+            "AS p(ingredient_name, prev_q6, prev_q5, prev_q4, prev_q3, prev_q2, prev_q1, quantity) WHERE i.ingredient_name = p.ingredient_name"  
+        );
+        console.log('Updating current_day in orders');
+        pool.query(
+            "UPDATE orders SET current_day = FALSE" 
+        );
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 // editInventory("chocolate", newQuantity = 415);
 
 exports.getInventory = getInventory;
 exports.editInventory = editInventory;
+exports.signalNewDay = signalNewDay;
--- src/js/Manager_Side.js
+++ src/js/Manager_Side.js
@@ -6,15 +6,93 @@ const Order = require("./Order");
 
 
 async function getMostUsedItems() {
-    console.log("Work in progress");
+    try {
+        console.log('Getting most used ingredients');
+        const res = await pool.query(
+            "SELECT ingredient_name, (prev_q1 - quantity) AS \"amt_used\" FROM inventory_data WHERE (prev_q1 - quantity) > 0 ORDER BY (prev_q1 - quantity) DESC"
+        );
+        console.log(res.rows);
+        return res.rows;
+    } catch (error) {
+        console.error(error)
+    }
 }
 
+getMostUsedItems();
+
 async function getSellsTogether() {
-    console.log("Work in progress");
+    try {
+        console.log('Getting items sold together');
+        const res = await pool.query(
+            "SELECT items_ordered FROM orders"
+        );
+        const freq = new Map();
+        for(let i = 0; i < res.rowCount; ++i) {
+            const items = res.rows[i].items_ordered;
+            for(let a = 0; a < items.length; ++a) {
+                let itemA = items[a];
+                for(let b = a + 1; b < items.length; ++b) {
+                    let itemB = items[b];
+                    if(itemA != itemB) {
+                        if(itemA > itemB) {
+                            let bubble = itemB;
+                            itemB = itemA;
+                            itemA = bubble;
+                        }
+                        let pair = itemA.toString() + "," + itemB.toString();
+                        if(freq.has(pair)) {
+                            freq.set(pair, freq.get(pair) + 1);
+                        } else {
+                            freq.set(pair, 1);
+                        }
+                    }
+                }
+            }
+        }
+        const freqArr = [];
+        for(const [key, value] of freq) {
+            //console.log(`${key} = ${value}`);
+            if(value > 1)
+                freqArr.push([key.split(",")[0], key.split(",")[1], value]);
+        }
+        freqArr.sort(arrayCompare);
+        //for(let i = 0; i < freqArr.length; ++i)
+            //console.log(freqArr[i][0] + " " + freqArr[i][1] + " " + freqArr[i][2]);
+        const itemNames = new Map();
+        const menu = await Item.getMenu();
+        for(let i = 0; i < menu.length; ++i) {
+            itemNames.set(menu[i].item_number, menu[i].item_name);
+        }
+        const sellsTogether = [];
+        for(let i = 0; i < freqArr.length; ++i) {
+            const entry = {"item_1": itemNames.get(parseInt(freqArr[i][0])), "item_2": itemNames.get(parseInt(freqArr[i][1])), "times_sold": freqArr[i][2]};
+            sellsTogether.push(entry);
+        }
+        console.log(sellsTogether);
+        return sellsTogether;
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function arrayCompare(a, b) {
+    if(a[2] === b[2])
+        return 0;
+    else
+        return (a[2] > b[2]) ? -1 : 1;
 }
 
 async function restockReport() {
-    console.log("Work in progress");
+    try {
+        console.log('Creating restock report');
+        const res = await pool.query(
+            "SELECT ingredient_name, (quantity || ' ' || units) AS \"quantity\", (min_q || ' ' || units) AS \"min_q\" FROM inventory_data WHERE quantity < min_q ORDER BY ingredient_name"
+        );
+        console.log(res.rows);
+        return res.rows;
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function salesReport() {
@@ -26,7 +104,18 @@ async function excessReport() {
 }
 
 async function xReport() {
-    console.log("Work in progress");
+    try {
+        console.log('Creating X report');
+        const res = await pool.query(
+            "SELECT menu_items.item_name, COUNT(orders.items_ordered) AS times_ordered FROM orders " + 
+            "INNER JOIN menu_items ON menu_items.item_number = ANY(orders.items_ordered) WHERE orders.current_day = TRUE " +
+            "GROUP BY UNNEST(orders.items_ordered), menu_items.item_number ORDER BY times_ordered DESC"
+        );
+        console.log(res.rows);
+        return res.rows;
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 async function zReport() {
@@ -45,5 +134,6 @@ exports.addItem = Item.addItem;
 exports.removeItem = Item.removeItem;
 exports.getInventory = Ingredient.getInventory;
 exports.editInventory = Ingredient.editInventory;
+exports.signalNewDay = Ingredient.signalNewDay;
 exports.employeeInfo = Employee.employeeInfo;
 exports.getSales = Order.getSales;
