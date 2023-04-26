e698674bfd381a48d48b776ccf8c4d39e4f9a4dd
Matthew Wang <mattwang@client-10-228-108-178.tamulink.tamu.edu>
2023-04-26 10:30:12 -0500

    commit message

--- RevGrillApp/client/src/App.js
+++ RevGrillApp/client/src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react'
+<<<<<<< HEAD
 import Signup from './pages/Log_In/signup'
+=======
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './pages/Manager_Side/Manager_Employee'
+import { Manager_Employee } from './pages/Manager_Side/Manager_Employee';
+import './pages/Manager_Side/manager.css';
+
+>>>>>>> 7bcbb3a0a989135ceaa3cffb9a8759b65097a66d
 function App() {
 
   const [hello, setHello] = useState('No data')
@@ -33,19 +41,21 @@ function App() {
 
   return (
     <div className="App">
+      <Manager_Employee></Manager_Employee>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>What are you doing here today?</h1>
+        
+        {/* <Router>
+        <ul>
+          <li><button href="/">Main</button></li>
+          <li><button href="/menu">Menu</button></li>
+          <li><button href="/customer">Customer</button></li>
+          <li><button href="/server">Server</button></li>
+          <li><button href="/server">Manager</button></li>
+        </ul>
+        <Route path="/" render={() => <h1>Welcome!</h1>} />
+        </Router> */}
         <p>{hello}</p>
         <p>{hello2}</p>
         <Signup/>
---
+++ RevGrillApp/client/src/pages/Manager_Side/Manager_Sales.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './manager.css';
+
+export class Manager_Sales extends Component {
+
+  render() {
+    return(
+      <div>
+
+        <body>
+            <div className="header">
+                <ul className="nav nav-ls">
+                    <div className="nav-home">
+                        <li><a className="nav-link link-home" href="">Rev's Grill</a></li>
+                    </div>
+                    <div className="nav-item">
+                        <li><a className="nav-link" href="Manager_Inventory.html">Inventory</a></li>
+                    </div>
+                    <div className="nav-item">
+                        <li><a className="nav-link" href="Manager_Employee.html">Menu</a></li>
+                    </div>
+                    <div className="nav-item">
+                        <li><a className="nav-link nav-curr" href="Manager_Sales.html">Sales</a></li>
+                    </div>
+                </ul>
+            </div>
+
+            <div className="ms-grid">
+                <div className="s-form">
+                    <h2>Select Report</h2>
+                    <p> form/buttons for report</p>
+                </div>
+                <div className="ms-display">
+                    <h2>Display Report</h2>
+                    <p>report will display here</p>
+                </div>
+            </div>
+        </body>
+
+        <footer>
+            Made with 🤍 by CSCE 315 Team 51
+        </footer>
+
+
+      </div>
+
+    
+    )
+  }
+
+}
+
