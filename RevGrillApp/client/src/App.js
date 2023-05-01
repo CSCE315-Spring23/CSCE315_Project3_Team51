931236574e2ee869c91c9f8e2eceeabfc311feca
Casey <91210043+peicasey@users.noreply.github.com>
2023-04-30 21:04:57 -0500

    changed so server side is app js

--- RevGrillApp/client/src/App.js
+++ RevGrillApp/client/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react'
 import Manager_Inventory from './pages/Manager_Side/Manager_Inventory';
+import Server_Categories from './pages/Server_Side/Server_Categories';
 
 import './pages/Manager_Side/manager.css';
 
@@ -38,7 +39,7 @@ function App() {
 
   return (
     <div className="App">
-      <Manager_Inventory></Manager_Inventory>
+      <Server_Categories></Server_Categories>
       
         <h1>What are you doing here today?</h1>
         
