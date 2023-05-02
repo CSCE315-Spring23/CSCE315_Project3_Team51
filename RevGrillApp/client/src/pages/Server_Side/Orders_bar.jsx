f4212cca882fa3f7c0a03d0bca59d2e2c897ace0
Alan Perez <prz.alan02@gmail.com>
2023-05-02 01:53:21 -0500

    changed url

--- RevGrillApp/client/src/pages/Server_Side/Orders_bar.jsx
+++ RevGrillApp/client/src/pages/Server_Side/Orders_bar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// const [orders, setOrders] = useState('No data - Orders')
-
-//     // fetch the information for the item given the number
-//     fetch("http://localhost:9000/orders")
-//       .then(r => r.text())
-//       .then(resp => {
-//         setOrders(resp)
-//     });
 
 export default function OrdersBar() {
+    const [orders, setOrders] = useState('No data - Orders')
 
-    
+
+    function getRunningOrders() {
+        fetch("https://revgrill-app.onrender.com/orders")
+            .then(r => r.text())
+            .then(resp => {
+                setOrders(resp)
+            });
+    }
+
+    // fetch the information for the item given the number
+    useEffect(() => {
+        getRunningOrders()
+    }, []);
 
     return (
         <div className="order_bar">
-            <h2> <span class="material-symbols-outlined">receipt_long</span> ONGOING ORDERS</h2>
-            {/* <p>{orders}</p> */}
+            <h2> <span class="material-symbols-outlined">receipt_long</span> CURRENT ORDERS</h2>
+            <p>{orders}</p>
         </div>
-    )
+    );
 }
