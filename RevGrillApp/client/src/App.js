4aac2e11f821009b8627aa2af9d1455c9be001c8
annabrookss <ambrooks.271@tamu.edu>
2023-05-01 22:10:57 -0500

    fixed size of menu and added customer options

--- RevGrillApp/client/src/App.js
+++ RevGrillApp/client/src/App.js
@@ -5,73 +5,80 @@ import Manager_Inventory from './pages/Manager_Side/Manager_Inventory';
 import './pages/Manager_Side/manager.css';
 
 
-function App() {
-
-  const [ingredients, setIngredients] = useState('No data - Ingredients')
-  const [orders, setOrders] = useState('No data - Orders')
-
-  const callAPIIngredients = () => {
-    fetch("http://localhost:9000/users")
-      .then(r => r.text())
-      .then(resp => {
-        setIngredients(resp)
-      });
-  }
-
-  const callAPIOrders = () => {
-    fetch("http://localhost:9000/orders")
-      .then(r => r.text())
-      .then(resp => {
-        setOrders(resp)
-      });
-  }
-
-  useEffect(() => {
-    callAPIIngredients()
-  }, [])
-
-  useEffect(() => {
-    callAPIOrders()
-  }, [])
-  
-
-  
-
-  return (
-    <div className="App">
-      <Manager_Inventory></Manager_Inventory>
-      
-        <h1>What are you doing here today?</h1>
-        
-        {/* <Router>
-        <ul>
-          <li><button href="/">Main</button></li>
-          <li><button href="/menu">Menu</button></li>
-          <li><button href="/customer">Customer</button></li>
-          <li><button href="/server">Server</button></li>
-          <li><button href="/manager">Manager</button></li>
-        </ul>
-        <Route path="/" render={() => <h1>Welcome!</h1>} />
-        <Route render={() => <h1>404: page not found</h1>} />
-        </Router> */}
-      
-        <p>{ingredients}</p>
-        <p>{orders}</p>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-// BELOW: FOR MENU
-//import Menu from './pages/Menu_Side/Menu';
-
 // function App() {
+
+//   const [ingredients, setIngredients] = useState('No data - Ingredients')
+//   const [orders, setOrders] = useState('No data - Orders')
+
+//   const callAPIIngredients = () => {
+//     fetch("http://localhost:9000/users")
+//       .then(r => r.text())
+//       .then(resp => {
+//         setIngredients(resp)
+//       });
+//   }
+
+//   const callAPIOrders = () => {
+//     fetch("http://localhost:9000/orders")
+//       .then(r => r.text())
+//       .then(resp => {
+//         setOrders(resp)
+//       });
+//   }
+
+//   useEffect(() => {
+//     callAPIIngredients()
+//   }, [])
+
+//   useEffect(() => {
+//     callAPIOrders()
+//   }, [])
+  
+
+  
+
 //   return (
-//     <Menu />
+//     <div className="App">
+//       <Manager_Inventory></Manager_Inventory>
+      
+//         <h1>What are you doing here today?</h1>
+        
+//         {/* <Router>
+//         <ul>
+//           <li><button href="/">Main</button></li>
+//           <li><button href="/menu">Menu</button></li>
+//           <li><button href="/customer">Customer</button></li>
+//           <li><button href="/server">Server</button></li>
+//           <li><button href="/manager">Manager</button></li>
+//         </ul>
+//         <Route path="/" render={() => <h1>Welcome!</h1>} />
+//         <Route render={() => <h1>404: page not found</h1>} />
+//         </Router> */}
+      
+//         <p>{ingredients}</p>
+//         <p>{orders}</p>
+//     </div>
 //   );
 // }
 
 // export default App;
+
+
+
+// BELOW: FOR MENU
+// import Menu2 from './pages/Menu_Side/Menu2';
+
+// function App() {
+//   return (
+//     <Menu2 />
+//   );
+// }
+
+// export default App;
+
+// BELOW : FOR CUSTOMER
+import Options from './pages/Customer_Side/Options';
+function App() {
+  return ( <Options category='Burger' />);
+}
+export default App;
