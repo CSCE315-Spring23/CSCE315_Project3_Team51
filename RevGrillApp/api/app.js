bff3f26751beed04a40c4975e5a62fb09692c568
Alan Perez <prz.alan02@gmail.com>
2023-05-02 18:04:13 -0500

    mes

--- RevGrillApp/api/app.js
+++ RevGrillApp/api/app.js
@@ -1,21 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var newRoute = require('./routes/newRoute');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const newRoute = require('./routes/newRoute');
 
 // tables
-var ordersRoute = require('./routes/order_table');
-var ingredientsRoute = require('./routes/ingredient_table');
-var itemsRoute = require('./routes/item_table');
-var employeesRoute = require('./routes/employee_table');
+const ordersRoute = require('./routes/order_table');
+const ingredientsRoute = require('./routes/ingredient_table');
+const itemsRoute = require('./routes/item_table');
+const employeesRoute = require('./routes/employee_table');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,12 +38,12 @@ app.use('/items', itemsRoute);
 app.use('/employees', employeesRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
