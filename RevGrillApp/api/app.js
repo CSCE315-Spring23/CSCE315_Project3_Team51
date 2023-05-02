81458b8ee338671593da7e8a339008e6322bd47d
Alan Perez <prz.alan02@gmail.com>
2023-05-02 00:52:14 -0500

    working version???

--- RevGrillApp/api/app.js
+++ RevGrillApp/api/app.js
@@ -53,4 +53,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 module.exports = app;
