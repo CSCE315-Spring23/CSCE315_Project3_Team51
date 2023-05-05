a36fa85f4085738021ccd44bfa5473688d534bd0
Casey <91210043+peicasey@users.noreply.github.com>
2023-05-05 06:44:07 -0500

    reformatted home page

--- RevGrillApp/client/src/pages/Home/Home.jsx
+++ RevGrillApp/client/src/pages/Home/Home.jsx
@@ -44,17 +44,17 @@ export default function Home() {
     return (
         <div style={{backgroundColor:"maroon"}}>
             <h1 className="intro-text">Welcome to Rev's!</h1>
-            <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', padding:'0px 50px 0px'}}>
+            <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', padding:'0% 10% 0% 5%'}}>
                 <img className='reasonable-size' src={Loading} alt="Reveille as a Rev's worker, flipping ingredients in a pan"/>
                 <div style={{width:40,}}></div>
                 <div>
                     <h1 className="better-header">Who We Are 🐕‍🦺</h1>
-                    <h2 className="description-text">
+                    <h2 style={{fontSize:'100'}} className="description-text">
                         A fun, casual dining area where you can grab a burger, root beer float, or catch the game. Invite your friends and relax on the outside patio!
                     </h2>
                 </div>
             </div>
-            <div style={{display:'flex', backgroundColor:'var(--custom-primary)', padding:20, padding:'0px 50px 0px'}}>
+            <div style={{display:'flex', backgroundColor:'var(--custom-primary)', gap:'10%', padding:'0% 10% 0%'}}>
                 <div>
                     <h2 className="better-header">Location</h2>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3433.7598451443373!2d-96.34332198907488!3d30.61253637453673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8646839855edbe95%3A0xa6c8cc70fe38a127!2sRev&#39;s%20American%20Grill!5e0!3m2!1sen!2sus!4v1683045370859!5m2!1sen!2sus" 
@@ -62,7 +62,6 @@ export default function Home() {
                     <h3 style={{lineHeight:0.5,}} className="description-text">275 Joe Routt Blvd, College Station, TX 77840</h3>
                     <h3 style={{lineHeight:0.5, paddingBottom:'20px',}} className="description-text">Located in Student Memorial Center (MSC)</h3>
                 </div>
-                <div style={{width:40,}}></div>
                 <div>
                     <h2 className="better-header">Hours of Operation</h2>
                     <h3 style={{lineHeight:0.5,}} className="description-text">
@@ -75,7 +74,6 @@ export default function Home() {
                         <p>Sunday - 10 AM to 8 PM</p>
                     </h3>
                 </div>
-                <div style={{width:40,}}></div>
                 <div>
                     <h2 className="better-header">Contact</h2>
                     <h3 className="description-text">
@@ -86,7 +84,7 @@ export default function Home() {
             </div>
             <div>
                 <h1 style={{color:'white', marginTop:'50px',}}>What are you doing with us today?</h1>
-                <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', gap:'10px', padding:'0px 150px 0px'}}>
+                <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', gap:'10px', padding:'0% 25%'}}>
                     <div href={`signup`} className="view-button">
                         <img style={{ height:'150px', width:'150px', }} src={ Customer_View } alt="customer icon" onClick = {goCustomer}/>
                         <h2 style={{lineHeight:'15px', marginTop:'5px', color:'white'}}>Customer</h2>
@@ -105,7 +103,7 @@ export default function Home() {
                     </div>
                     
                 </div>
-                <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', gap:'10px', padding:'0px 200px 0px'}}>
+                <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', gap:'5%', padding:'0% 25%'}}>
                     <div>
                         <h2 style={{color:"var(--custom-secondary)", marginBottom:"0px",}}>Have an account?</h2>
                         <button class="button-57" role="button"><span class="text">log-in</span><span onClick={goLogin}>logging in...</span></button>
